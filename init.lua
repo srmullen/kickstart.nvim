3a2523517b688407ed5f3874621143be3fcfcbe3
Sean Mullen <srmullen13@gmail.com>
2025-01-16 15:44:28 -0500

    move themes to their own configuration file

--- init.lua
+++ init.lua
@@ -848,36 +848,7 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    -- vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    -- vim.cmd.hi 'Comment gui=none'
-    -- end,
-  },
-
-  {
-    'shaunsingh/nord.nvim',
-    enabled = true,
-    priority = 1000,
-    config = function()
-      vim.cmd [[colorscheme nord]]
-    end,
-  },
-
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-
-  {
+   {
     'numToStr/Comment.nvim',
     opts = {
       -- add any options here
@@ -994,8 +965,10 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'custom.plugins.colorthemes',
+
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
