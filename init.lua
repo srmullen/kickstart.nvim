44eb4b5e81f22fc432c55185e53497999e9dc283
Sean Mullen <srmullen13@gmail.com>
2025-01-17 15:06:45 -0500

    add harpoon

--- init.lua
+++ init.lua
@@ -969,6 +969,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.colorthemes',
   require 'custom.plugins.lualine',
+  require 'plugins.harpoon',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
