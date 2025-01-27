b7b60de294b8ec91a7b287c61cbc74c9da1f9ee3
Sean Mullen <srmullen13@gmail.com>
2025-01-27 10:17:16 -0500

    enable cursor blinking in insert mode

--- init.lua
+++ init.lua
@@ -116,6 +116,8 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Enable cursor blinking
+vim.opt.guicursor = 'n-v-c:block,i-ci:ver25-Cursor/lCursor-blinkon100'
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
