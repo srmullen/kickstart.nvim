a86f50ab6deaf8fa3abc59890967063f8934937a
Sean Mullen <srmullen13@gmail.com>
2025-02-08 12:54:59 -0500

    hover plugin

--- init.lua
+++ init.lua
@@ -988,6 +988,7 @@ require('lazy').setup({
   require 'custom.plugins.lualine',
   require 'plugins.harpoon',
   require 'plugins.obsidian',
+  require 'plugins.hover',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
