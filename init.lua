d67f8de43d7f49b2c38028d8bb4bc2c8e9fcbe00
Sean Mullen <srmullen13@gmail.com>
2025-02-13 14:07:20 -0500

    install vim fugitive

--- init.lua
+++ init.lua
@@ -994,6 +994,7 @@ require('lazy').setup({
   require 'plugins.hover',
   require 'plugins.undotree',
   require 'plugins.aerial',
+  require 'plugins.git',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
