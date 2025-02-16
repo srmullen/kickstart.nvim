3d000e8af68cfbf6cd959acdb471ad789ca05f87
Sean Mullen <srmullen13@gmail.com>
2025-02-16 10:31:59 -0500

    add ollama as a provider. Switching to it didn't seem to work though

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -24,6 +24,14 @@ return {
       max_tokens = 4096,
       timeout = 30000,
     },
+    vendors = {
+      ollama = {
+        __inherited_from = 'openai',
+        api_key_name = '',
+        endpoint = 'http://127.0.0.1:11434/v1',
+        model = 'deepseek-r1:14b',
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make BUILD_FROM_SOURCE=true',
