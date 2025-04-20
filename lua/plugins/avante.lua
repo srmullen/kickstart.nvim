0a7cb93ab7fa50f806c1820187dd07827418b222
Sean Mullen <srmullen13@gmail.com>
2025-04-20 09:18:02 -0400

    fix avante after update

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -34,9 +34,11 @@ return {
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- build = 'make',
   build = 'make BUILD_FROM_SOURCE=true',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
